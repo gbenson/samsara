49348242e7a7fa6007e897e83733f73c661c0180
gary <devnull@localhost>
2007-05-01 13:51:58 +0000

    Make sure there is only one xmlns="..." and that it's right

--- samsara/handlers/markup.py
+++ samsara/handlers/markup.py
@@ -5,6 +5,8 @@ class MarkupHandler(server.HandlerClass):
     """
     priority = -50
 
+    xhtmlns = "http://www.w3.org/1999/xhtml"
+
     def handle(self, r):
         if r.type == "text/xml":
             node = r.payload.children
@@ -24,4 +26,7 @@ class MarkupHandler(server.HandlerClass):
             finally:
                 doc.freeDoc()
 
-            assert r.payload.find('xmlns=""') == -1
+            hunt = 'xmlns="%s"' % self.xhtmlns
+            found = r.payload.find('xmlns="')
+            assert found != -1 and r.payload[found:found + len(hunt)] == hunt
+            assert r.payload.find('xmlns="', found + len(hunt)) == -1
