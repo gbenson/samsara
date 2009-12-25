085c09b114a3bfb30fd0b6bb079fa97bc7aa6a47
Gary Benson <gbenson@redhat.com>
2009-12-25 14:47:40 +0000

    This hack has no place here

--- samsara/handlers/serialize.py
+++ samsara/handlers/serialize.py
@@ -5,19 +5,10 @@ class XMLSerializer(server.HandlerClass):
     """
     priority = -75
 
-    xhtmlns = "http://www.w3.org/1999/xhtml"
-    rubbish = 'xmlns:ssr="http://inauspicious.org/samsara"'
-
     def handle(self, r):
         if r.type != "text/xml":
             return
         doc = r.payload
-        r.payload = doc.serialize("utf-8", 1).replace(" " + self.rubbish, "")
+        r.payload = doc.serialize("utf-8", 1)
         r.type = "text/html"
         doc.freeDoc()
-
-        if not r.payload.rstrip().endswith("</rss>"):
-            hunt = 'xmlns="%s"' % self.xhtmlns
-            found = r.payload.find('xmlns')
-            assert found != -1 and r.payload[found:found + len(hunt)] == hunt
-            assert r.payload.find('xmlns', found + len(hunt)) == -1
