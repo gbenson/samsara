89bebf8f56f5c2e6575da461690afab881c88e67
Gary Benson <gbenson@redhat.com>
2010-07-21 00:48:55 +0100

    Add an "S" button to pages when running in the HTTP server

---
+++ samsara/handlers/button.py
@@ -0,0 +1,42 @@
+from samsara import server
+
+class SamsaraButton(server.HandlerClass):
+    """Add a Samsara button to pages when running in the HTTP server
+    """
+    priority = -76
+
+    html = ('<div style="%s;">' % "; ".join(
+            ("position: absolute",
+             "top: 3px",
+             "right: 3px",
+             "width: 16px",
+             "height: 16px",
+             "border: 2px solid white",
+             "background-color: #eee",
+             "font-family: arial, helvetica, sans-serif",
+             "font-size: 14px",
+             "font-weight: bold",
+             "text-align: center")),
+            '  <a style="%s" href="/samsara/">S</a>' % "; ".join(
+            ("text-decoration: none",
+             "color: white")),
+            '</div>')
+
+    def handle(self, r):
+        if self.server.httpserver is None:
+            return
+        if r.type != "text/html":
+            return
+
+        payload = r.getPayload()
+        start = payload.lower().find("<body")
+        if start < 0:
+            return
+        start = payload.find(">", start)
+        start += 1
+        limit = start
+        while payload[limit].isspace():
+            limit += 1
+        sep = payload[start:limit]
+        r.payload = sep.join(
+            (payload[:start],) + self.html + (payload[limit:],))
