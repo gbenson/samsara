d23d149bad75ec51dc15e225def0fe520055ee05
Gary Benson <gbenson@redhat.com>
2011-02-04 15:36:57 +0100

    Modernize

--- samsara/spider.py
+++ samsara/spider.py
@@ -3,29 +3,30 @@ import os
 def spider(server, dest, startpoints, exclusions = ()):
     items = {}
     for path in startpoints:
-        items[path] = 0
+        items[path] = False
 
-    while 1:
-        todo = filter(lambda i: not items[i], items.keys())
+    while True:
+        todo = [path for path, done in items.items() if not done]
         if not todo:
             break
 
         for path in todo:
-            items[path] = 1
+            items[path] = True
             response = server.get(path)
             server.auto_update = False
             server.xmlctx.xpathctx.auto_update = False
 
-            links = map(lambda l: l.normalizeTo(path),
-                        filter(lambda l: l.isLocal(),
-                               response.links()))
+            links = [link.normalizeTo(path)
+                     for link in response.links()
+                     if link.isLocal()]
+
             for link in links:
                 if not items.has_key(link):
                     for excl in exclusions:
                         if link[:len(excl)] == excl:
                             break
                     else:
-                        items[link] = 0
+                        items[link] = False
 
             if path[-1] == "/":
                 payload = response.payload
@@ -34,7 +35,7 @@ def spider(server, dest, startpoints, exclusions = ()):
                 else:
                     path += "index.html"
             path = os.path.join(dest, path[1:])
-            dir = os.path.split(path)[0]
+            dir = os.path.dirname(path)
             if not os.path.isdir(dir):
                 os.makedirs(dir)
             response.writePayload(path)
