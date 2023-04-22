a37db3e2fb99d5878659123a821d68e013a74838
Gary Benson <gary@gbenson.net>
2023-04-22 23:17:16 +0100

    Install /etc/mime.types

--- docker/setup_base.sh
+++ docker/setup_base.sh
@@ -5,7 +5,7 @@ echo 'assumeyes=1' >> /etc/yum.conf
 yum install deltarpm
 yum update
 
-yum install libxml2-python libxslt-python python-virtualenv
+yum install libxml2-python libxslt-python python-virtualenv mailcap
 
 yum clean all
 rm -rf /usr/share/doc/*
