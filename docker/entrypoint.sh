2fff731e1516d51a6a676983ed0b850591ed22d8
Gary Benson <gary@gbenson.net>
2023-04-23 00:16:53 +0100

    Alias unittest as test, and add --help

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
 
+entry="$1"
+shift
+
+case "$entry" in
+  test)
+    entry=unittest
+    ;;
+  *help)
+    cat >&2 <<EOF
+Usage: samsara <command> [options]
+
+Commands:
+  get     Perform a single request, writing the response to stdout
+  server  Start an HTTP server on port 2420 and serve forever
+  spider  Spider a site, writing static files to the specified directory
+  test    Run the bundled unit tests
+
+EOF
+    exit
+esac
+
 . /venv/bin/activate
 
-exec "$@"
+exec "$entry" "$@"
